2af67c5118482b0faed8b4bc22fe01b42ac3256e
sushildangi <dangi.sushil5@gmail.com>
2017-12-04 22:26:14 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
